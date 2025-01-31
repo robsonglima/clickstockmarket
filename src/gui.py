aa161f7b435397ba7c65af60c84e9ed8ac6c60b8
robsonglima <robsonglima@gmail.com>
2025-01-31 14:45:30 +0000

    cut precos_intradiarios_top_15 df to updated

--- src/gui.py
+++ src/gui.py
@@ -25,12 +25,11 @@ if page == "Principal":
 
 elif page == "Gráfico":    
     st.title("Análise do Gráfico")    
-    data_frame_top_15_industry, data_frame_precos_intradiarios, tickers_top_15 = load_data()
-    if isinstance(data_frame_top_15_industry, pd.DataFrame):
-        tickers_top_15 = data_frame_top_15_industry['TckrSymb'].tolist()    
-        run_app(data_frame_top_15_industry, data_frame_precos_intradiarios,tickers_top_15)
+    data_frame_top_15_industry, data_frame_precos_intradiarios, tickers_top_15 = load_data()    
+    run_app(data_frame_top_15_industry, data_frame_precos_intradiarios,tickers_top_15)
 
 elif page == "Tabela":
+
     st.title("Tabelas de Dados")
     
     interval_options = ["1min", "2min", "5min", "15min", "30min", "60min", "90min", "1h", "1d", "5d", "1wk", "1mo", "3mo"]
@@ -39,10 +38,11 @@ elif page == "Tabela":
     period = st.selectbox("Selecione Periodo", period_options, index=5)
     
     data_frame_top_15_industry, data_frame_precos_intradiarios, tickers_top_15 = load_data()    
-
+    
     if isinstance(data_frame_top_15_industry, pd.DataFrame):
-        tickers_top_15 = data_frame_top_15_industry['TckrSymb'].tolist()
         if st.button("Atualizar"):
+            tickers_top_15 = data_frame_top_15_industry['TckrSymb'].tolist()
+
             try:
                 with st.spinner("Atualizando..."):
                     data_frame_precos_intradiarios = update_data_frames(tickers_top_15, interval, period)
@@ -50,6 +50,8 @@ elif page == "Tabela":
             except Exception as e:
                 st.error(f"Ops, houve um erro ao atualizar: {e}")
     # Display tables
+            data_frame_precos_intradiarios = pd.read_csv("src/precos_intradiarios_top_15.csv")
+            tickers_top_15 = data_frame_precos_intradiarios['symbol'].unique().tolist()
 
     if isinstance(data_frame_top_15_industry, pd.DataFrame):
         st.subheader("Top 15 Empresas Listadas")
