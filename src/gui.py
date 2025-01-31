0e08e1288df2dffb1f82b0ea947b9b8940c16086
robsonglima <robsonglima@gmail.com>
2025-01-31 02:49:32 +0000

    I added the code to update the app when change the period or interval, in the table page

--- src/gui.py
+++ src/gui.py
@@ -2,14 +2,14 @@ import streamlit as st
 from app import load_data, run_app, display_top_15_table, display_intraday_prices_table
 from analitics import consultar_precos_intradiarios_yf
  
-
+data_frame_top_15_industry, data_frame_precos_intradiarios = load_data()
 st.sidebar.title("Navigation")
 page = st.sidebar.radio(
-    "Go to", ["Home", "App", "Tabela"]
+    "Go to", ["Home", "App", "Table"]
 )  # Updated menu options and page names
 
 # --- Page Content ---
-if page == "Home":
+if page == "Home":    
     st.title("Stock Market Analysis")  # Changed title
     st.markdown(
         """
@@ -25,6 +25,23 @@ if page == "Home":
         st.write("Home!")
 
 
+elif page == "App":
+    st.title("App")
+    # User input for interval and period
+    interval_options = ["1m", "2m", "5m", "15m", "30m", "60m", "90m", "1h", "1d", "5d", "1wk", "1mo", "3mo"]
+    period_options = ["1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"]
+    interval = st.selectbox("Select Interval", interval_options, index=7)  # Default to "1h"
+    period = st.selectbox("Select Period", period_options, index=5)  # Default to "1y"
+    
+        
+    # Filter the data based on the selected period and interval
+    df_filtered = data_frame_precos_intradiarios.copy()
+    if period != "max":
+        df_filtered = df_filtered.last(period)
+    run_app(data_frame_top_15_industry, df_filtered, interval, period)
+
+elif page == "Table":
+    st.title("Data Tables")
     # Button to update data from yfinance
     if st.button("Update Data"):
         try:
@@ -34,33 +51,8 @@ if page == "Home":
                 updated_df_precos_intradiarios = consultar_precos_intradiarios_yf(tickers_top_15, interval, period)
                 data_frame_precos_intradiarios = updated_df_precos_intradiarios
             st.success("Data updated successfully!")
-
-
-
-        except Exception as e:
-
-
-            st.error(f"An error occurred while updating the data: {e}")
-elif page == "App":
-    st.title("App")
-    data_frame_top_15_industry, data_frame_precos_intradiarios = load_data()
-    # User input for interval and period
-    interval_options = ["1m", "2m", "5m", "15m", "30m", "60m", "90m", "1h", "1d", "5d", "1wk", "1mo", "3mo"]
-    period_options = ["1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"]
-    interval = st.selectbox("Select Interval", interval_options, index=7)  # Default to "1h"
-    period = st.selectbox("Select Period", period_options, index=5)  # Default to "1y"
     
     # Filter the data based on the selected period and interval
-    df_filtered = data_frame_precos_intradiarios.copy()
-    if period != "max":
-        df_filtered = df_filtered.last(period)
-
-    run_app(data_frame_top_15_industry, df_filtered, interval, period)
-
-elif page == "Tabela":
-    st.title("Data Tables")
-    data_frame_top_15_industry, data_frame_precos_intradiarios = load_data()
-    
     # Display tables
     if data_frame_top_15_industry is not None:
         st.subheader("Top 15 Companies")
@@ -69,7 +61,14 @@ elif page == "Tabela":
     if data_frame_precos_intradiarios is not None:
         st.subheader("Intraday Prices")
         display_intraday_prices_table(data_frame_precos_intradiarios)
-        
+    # User input for interval and period
+    interval_options = ["1m", "2m", "5m", "15m", "30m", "60m", "90m", "1h", "1d", "5d", "1wk", "1mo", "3mo"]
+    period_options = ["1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"]
+    interval = st.selectbox("Select Interval", interval_options, index=7)  # Default to "1h"
+    period = st.selectbox("Select Period", period_options, index=5)  # Default to "1y"
+    
+    
+   
 
 
 
