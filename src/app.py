94f75c2162932e1f3182d72cfd361e555ae3f0e7
robsonglima <robsonglima@gmail.com>
2025-01-31 19:55:49 +0000

    cards/trla/avg variation

--- src/app.py
+++ src/app.py
@@ -10,13 +10,13 @@ from analitics import consultar_precos_intradiarios_yf
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def show_company_info(company_data, ticker):
-    """
-    Exibe informações da empresa e um gráfico de linha para um determinado ticker.
+    """Exibe informações da empresa e um gráfico de linha para um determinado ticker.
 
     Args:
         company_data (dict): Um dicionário contendo os dados da empresa.
         ticker (str): O símbolo do ticker.
     """
+    
     st.subheader(f"Informaçoes da empresa: {ticker}")
     
     if "profile" in company_data and company_data["profile"] != "N/A":
@@ -49,13 +49,12 @@ def show_company_info(company_data, ticker):
 
 # def para carregar Dataframes
 def load_data():
-    """
-    Loads the DataFrames from CSV files and extracts the top 15 tickers.
+    """Carrega os DataFrames a partir de arquivos CSV e extrai os 15 principais tickers.
 
     Returns:
-        tuple: A tuple containing the df_top_15_industry DataFrame,
-               the df_precos_intradiarios DataFrame, and a list of
-               the top 15 tickers. or list with tickers.
+        tuple: Uma tupla contendo o DataFrame df_top_15_industry,
+               o DataFrame df_precos_intradiarios e uma lista dos
+               15 principais tickers. ou lista com os tickers.
     """
     logging.info("Attempting to load data...")
     csv_top_15_industry_path = "src/df_top_15_com_industry.csv"
@@ -72,9 +71,9 @@ def load_data():
     df_top_15_industry = pd.read_csv(csv_top_15_industry_path, sep=";")
     try:
 
-        df_precos_intradiarios = pd.read_csv(csv_precos_intradiarios_path)
+      df_precos_intradiarios = pd.read_csv(csv_precos_intradiarios_path)
     except pd.errors.EmptyDataError:
-        return None, None, None
+      return None, None, None
 
     
     logging.info("Data loaded successfully.")
@@ -90,17 +89,16 @@ def load_data():
 
 
 def update_data_frames(tickers, interval, period):
-    """
-    Updates the DataFrames by consulting intraday prices for the given tickers.
+    """Atualiza os DataFrames consultando os preços intradiários para os tickers fornecidos.
 
     Args:
-        tickers (list): A list of stock tickers to update.
-        interval (string): the interval of time to consult
-        period (string): the period of time to consult
+        tickers (list): Uma lista de tickers de ações para atualizar.
+        interval (string): o intervalo de tempo para consultar
+        period (string): o período de tempo para consultar
     Returns:
-        tuple: A tuple containing the df_top_15_industry DataFrame, and the 
-               updated df_precos_intradiarios DataFrame.
-    """
+        tuple: Uma tupla contendo o DataFrame df_top_15_industry e o
+               DataFrame df_precos_intradiarios atualizado.
+    """    
     logging.info("Attempting to update data frames...")
     if not tickers:
         logging.error("Could not retrieve tickers.")
@@ -123,7 +121,7 @@ def update_data_frames(tickers, interval, period):
 # def para montar tabelas
 # def para montar tabela - top 15 companies table
 def display_top_15_table(df):
-    """Displays the table of top 15 companies."""
+    """Exibe a tabela das 15 principais empresas."""
     logging.info("Displaying top 15 companies table...")
     if df is not None:
         logging.info(f"DataFrame content for display_top_15_table():\n{df.head()}")
@@ -135,7 +133,7 @@ def display_top_15_table(df):
         logging.error("DataFrame vazio")
 
 def display_intraday_prices_table(df):
-    """Displays the intraday prices table."""
+    """Exibe a tabela de preços intradiários."""
     logging.info("Displaying intraday prices table...")
     if df is not None:
         logging.info(f"DataFrame content for display_intraday_prices_table():\n{df.head()}")
@@ -149,13 +147,34 @@ def display_intraday_prices_table(df):
 # def para rodar o app
 # Main app structure
 def run_app(df_top_15_industry, df_precos_intradiarios, tickers_top_15):
-    """Main function to run the Streamlit app."""
-    st.title("Análise do Mercado de Ações")
-    logging.info("Starting Streamlit app...")
-    
-    
-    tickers_list = df_precos_intradiarios['symbol'].unique().tolist() if not df_precos_intradiarios.empty else []
+    """Função principal para executar o aplicativo Streamlit.
 
+    Returns:
+    list: uma lista com 9 valores."""
+    if df_precos_intradiarios.empty:
+        return [None, None, None, None, None, None, None, None, None]
+
+    # calcualr a media daily variation
+    daily_variation = df_precos_intradiarios.groupby('symbol')['close'].diff().mean()
+    avg_daily_variation = daily_variation
+    avg_daily_variation_str = "+" if avg_daily_variation >= 0 else "-"
+    avg_daily_variation_type = "positive" if avg_daily_variation >= 0 else "negative"
+
+    # Calculate the average volume
+    avg_volume = df_precos_intradiarios['volume'].mean()
+    avg_volume_diff = df_precos_intradiarios['volume'].diff().mean()
+    avg_volume_str = "+" if avg_volume_diff >= 0 else "-"
+    avg_volume_type = "positive" if avg_volume_diff >= 0 else "negative"
+
+    # Calculate standard deviation of daily variation
+    std_dev_daily_variation = df_precos_intradiarios.groupby('symbol')['close'].diff().std()
+    std_dev_daily_variation_diff = std_dev_daily_variation.mean()
+    std_dev_daily_variation_str = "+" if std_dev_daily_variation_diff >= 0 else "-"
+    std_dev_daily_variation_type = "positive" if std_dev_daily_variation_diff >= 0 else "negative"
+    
+    st.title("Analise do Mercado de Ações")
+    tickers_list = df_precos_intradiarios['symbol'].unique().tolist() if not df_precos_intradiarios.empty else []
+    
     
     if df_top_15_industry is not None and df_precos_intradiarios is not None:        
         logging.info("DataFrames loaded successfully. Displaying content.")
@@ -170,13 +189,13 @@ def run_app(df_top_15_industry, df_precos_intradiarios, tickers_top_15):
             else :
                 st.error("Falha ao atualizar os dados.")
                 logging.error("Falha ao atualizar os dados.")
-            
-                
-        # grafico de linha - Time Series
-        st.subheader("Série Temporal do Preço do Ticker") #Time Series
+        
+        # gráfico de linha - Time Series
+        st.subheader("Série Temporal do Preço do Ticker") #Série Temporal
         unique_tickers = df_precos_intradiarios['symbol'].unique()
-        selected_ticker = st.selectbox("Selecione o Ticker", sorted(unique_tickers)) #select box
+        selected_ticker = st.selectbox("Selecione o Ticker", sorted(unique_tickers))
         if selected_ticker:
+
             # Filtra os dados para o ticker selecionado
             ticker_data = df_precos_intradiarios[df_precos_intradiarios['symbol'] == selected_ticker].copy()
             # Calcula a cor da linha com base no primeiro e último preço
@@ -185,8 +204,10 @@ def run_app(df_top_15_industry, df_precos_intradiarios, tickers_top_15):
             if last_price >= first_price:
                 line_color = 'green'
             else:
-                line_color = 'red'
+                line_color = 'red'            
             fig = px.line(ticker_data, x='datetime', y='close', title=f'Time Series for {selected_ticker}', color_discrete_sequence=[line_color])
+
+            logging.info("Starting Streamlit app...")
             st.plotly_chart(fig)
 
     else:
@@ -194,13 +215,13 @@ def run_app(df_top_15_industry, df_precos_intradiarios, tickers_top_15):
 
 def show_comparative_graph(selected_tickers, start_date, end_date):
     """
-    Exibe um gráfico de linha comparativo dos preços de fechamento dos tickers selecionados.
+    Exibe um gráfico de linha comparativo dos preços de fechamento dos tickers selecionados.    
 
     Args:
         selected_tickers (list): Uma lista de símbolos de ticker.
         start_date (date): A data de início para os preços históricos.
         end_date (date): A data de término para os preços históricos.
-    """    
+    """
     if not selected_tickers:
         return
     # Adiciona o disclaime dos precos normalizados
@@ -231,7 +252,7 @@ def show_comparative_graph(selected_tickers, start_date, end_date):
             except Exception as e:
                 st.error(f"Error fetching data for {ticker}: {e}")
 
-        #  graph comparativo
+        #  graficos comparativo
         if all_data:
             comparison_df = pd.DataFrame(all_data)
             fig = px.line(comparison_df, title='Comparação de Preços Normalizados')
@@ -241,57 +262,47 @@ def show_comparative_graph(selected_tickers, start_date, end_date):
             st.write("Selecione dois ou mais tickers para comparar")
 
 def analyze_trend_initiation(selected_tickers, start_date, end_date):
-    """
-    Analisa o início de tendências de alta e baixa para os tickers selecionados.
+    """Analisa o início de tendências de alta e baixa para os tickers selecionados.
 
     Args:
         selected_tickers (list): Uma lista de símbolos de ticker.
         start_date (date): A data de início para a busca de dados.
         end_date (date): A data de término para a busca de dados.
-    
+
     Returns:
         tuple: Uma tupla contendo dois dicionários:
                - downward_trends (dict): Ticker e hora para a primeira tendência de baixa.
-               - upward_trends (dict): Ticker e hora para a primeira tendência de alta.
-    """
+               - upward_trends (dict): Ticker e hora para a primeira tendência de alta."""
     downward_trends = {}
     upward_trends = {}
 
-    for ticker in selected_tickers: 
-        try: 
-            # Fetch minute-by-minute data
-            data = yf.download(ticker, start=start_date, end=end_date, interval="1m")
+    for ticker in selected_tickers:
+      try:
+        # Fetch minute-by-minute data
+        data = yf.download(ticker, start=start_date, end=end_date, interval="1m")
 
-            if data.empty:
-                st.warning(f"Não há dados de tendência para {ticker} no intervalo selecionado.")
-                continue
+        if data.empty:
+          st.warning(f"No data found for {ticker} in the given time frame.")
+          continue
 
-            # Find the first downward trend
-            first_downward_trend_time = None
-            for i in range(1, len(data)):
-                if data['Close'].iloc[i] < data['Close'].iloc[i - 1]:
-                    first_downward_trend_time = data.index[i]
-                    break
-            
-            if first_downward_trend_time is not None:                
-                downward_trends[ticker] = first_downward_trend_time
-                
-                
-            # Find the first upward trend
-            first_upward_trend_time = None
-            for i in range(1, len(data)):
-                if data['Close'].iloc[i] > data['Close'].iloc[i - 1]:
-                    first_upward_trend_time = data.index[i]
-                    break
+        # Find the first downward trend
+        first_downward_trend_time = None
+        for i in range(1, len(data)):
+          if data['Close'].iloc[i] < data['Close'].iloc[i - 1]:
+            first_downward_trend_time = data.index[i]
+            first_downward_trend_time_formatted = data.index[i].strftime('%Y-%m-%d %H:%M')
+            downward_trends[ticker] = first_downward_trend_time_formatted
+            break
 
-            if first_upward_trend_time is not None:
-                upward_trends[ticker] = first_upward_trend_time            
-
-        except Exception as e:
+        # Find the first upward trend
+        first_upward_trend_time = None
+        for i in range(1, len(data)):
+          if data['Close'].iloc[i] > data['Close'].iloc[i - 1]:
+            first_upward_trend_time = data.index[i]
+            first_upward_trend_time_formatted = data.index[i].strftime('%Y-%m-%d %H:%M')
+            upward_trends[ticker] = first_upward_trend_time_formatted
+            break
+      except Exception as e:
             st.error(f"Falha ao analisar tendências para {ticker}: {e}")
+    return downward_trends, upward_trends
 
-    # Format the output to display date and time
-    formatted_downward_trends = {ticker: time.strftime('%Y-%m-%d %H:%M') for ticker, time in downward_trends.items()}
-    formatted_upward_trends = {ticker: time.strftime('%Y-%m-%d %H:%M') for ticker, time in upward_trends.items()}
-
-    return formatted_downward_trends, formatted_upward_trends
--- src/gui.py
+++ src/gui.py
@@ -1,13 +1,65 @@
 import streamlit as st
-from app import load_data, run_app, display_top_15_table, display_intraday_prices_table, update_data_frames, show_comparative_graph, analyze_trend_initiation, show_company_info
+from app import *
 
 from analitics import get_company_data
 import pandas as pd
 from datetime import date
 
+from typing import List
 
 st.sidebar.title("Menu")
-page = st.sidebar.radio("Ir para", ["Principal", "Comparativo", "Gráfico", "Tabela"])
+
+def display_cards(stats):
+    """
+    Exibe três cards com estatísticas.
+
+    Args:
+        stats (list): Uma lista de dicionários, onde cada dicionário contém o
+                      'name', 'stat', 'change' e 'changeType' para um card.
+    """
+    card_data = [
+        {
+            'name': 'Média de Variação Diária',
+            'stat': stats[0],
+            'change': stats[1],
+            'changeType': stats[2]
+        },
+        {
+            'name': 'Volume Médio Diário',
+            'stat': stats[3],
+            'change': stats[4],
+            'changeType': stats[5],
+        },
+        {
+            'name': 'Desvio Padrão',
+            'stat': stats[6],
+            'change': stats[7],
+            'changeType': stats[8]
+        }
+    ]
+    
+    card_html = '<dl class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">'
+    for item in card_data:
+      card_html += f'''
+      <div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+        <div class="flex items-center justify-between">
+          <dt class="truncate text-sm font-medium text-gray-500">{item['name']}</dt>
+          <dd class="ml-2 flex items-baseline">
+          <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset {'bg-emerald-100 text-emerald-800 ring-emerald-600/10 dark:bg-emerald-400/10 dark:text-emerald-500 dark:ring-emerald-400/20' if item['changeType'] == 'positive' else 'bg-red-100 text-red-800 ring-red-600/10 dark:bg-red-400/10 dark:text-red-500 dark:ring-red-400/20'}">
+            {item['change']}
+          </span>
+            </dd>
+        </div>
+        <div class="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
+          <dd class="text-3xl font-semibold text-gray-900">
+          {item['stat']}
+          </dd>
+        </div>
+      </div>
+      '''
+    card_html += '</dl>'
+    st.markdown(card_html, unsafe_allow_html=True)
+page = st.sidebar.radio("Navegar para:", ["Principal", "Comparativo", "Gráfico", "Tabela"])
 
 if page == "Principal":
 
@@ -18,11 +70,12 @@ if page == "Principal":
 
             Descubra as correlações entre ações e identifique padrões que podem indicar boas oportunidades de investimento. Nossa plataforma permite que você analise o comportamento do mercado em diferentes períodos de tempo, ajudando você a entender como os ativos se movem juntos e quais sinais podem antecipar tendências.
 
-            Use o menu para explorar os dados e melhorar suas estratégias.
+            Use a barra lateral para explorar os dados e melhorar suas estratégias.
 
         """
         )
     if st.button("Recarregar Principal"):
+        
         st.rerun()    
 elif page == "Comparativo":
     st.title("Comparativo")
@@ -49,28 +102,35 @@ elif page == "Comparativo":
     
     
         downward_trends, upward_trends = analyze_trend_initiation(selected_tickers, start_date, end_date)
+
+        if downward_trend:
+              for ticker, trend_time in downward_trends.items():
+                st.write(f"**Primeira Tendência de Baixa Iniciada:** {ticker} em {trend_time}")
+        else:
+                st.write(f"**Primeira Tendência de Baixa Iniciada:** Sem tendências detectadas.")
+
+        if upward_trend:
+              for ticker, trend_time in upward_trend.items():
+                st.write(f"**Primeira Tendência de Alta Iniciada:** {ticker} em {trend_time}")
+        else:
+              st.write(f"**Primeira Tendência de Alta Iniciada:** Sem tendências detectadas.")
+
         
-        if downward_trends:
-            for ticker, time in downward_trends.items():
-                st.write(f"**Primeira Tendência de Baixa Iniciada:** {ticker} às {time.strftime('%d/%m/%Y %H:%M')}")
-
-        if upward_trends:
-            for ticker, time in upward_trends.items():
-                st.write(f"**Primeira Tendência de Alta Iniciada:** {ticker} às {time.strftime('%d/%m/%Y %H:%M')}")
-
 elif page == "Gráfico":
     st.title("Análise do Gráfico")
     data_frame_top_15_industry, data_frame_precos_intradiarios, tickers_top_15 = load_data()
-    run_app(data_frame_top_15_industry, data_frame_precos_intradiarios,tickers_top_15)
+    stats = run_app(data_frame_top_15_industry, data_frame_precos_intradiarios,tickers_top_15)
+    display_cards(stats)    
+
 
 elif page == "Tabela":
 
     st.title("Tabelas de Dados")
 
-    interval_options = ["1min", "2min", "5min", "15min", "30min", "60min", "90min", "1h", "1d", "5d", "1wk", "1mo", "3mo"]
-    period_options = ["1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"]
-    interval = st.selectbox("Selecione Intervalo", interval_options, index=8)
-    period = st.selectbox("Selecione Periodo", period_options, index=5)
+    interval_options = ["1min", "2min", "5min", "15min", "30min", "60min", "90min", "1h", "1d"]
+    period_options = ["1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max",]
+    interval = st.selectbox("Selecione o Intervalo", interval_options, index=8)
+    period = st.selectbox("Selecione o Período", period_options, index=5)
 
     data_frame_top_15_industry, data_frame_precos_intradiarios, tickers_top_15 = load_data()
 
@@ -79,15 +139,15 @@ elif page == "Tabela":
             tickers_top_15 = data_frame_top_15_industry['TckrSymb'].tolist()
             try:
                 with st.spinner("Atualizando..."):
-                    data_frame_precos_intradiarios= update_data_frames(tickers_top_15, interval, period)
+                    data_frame_precos_intradiarios = update_data_frames(tickers_top_15, interval, period)
                     st.success("Atualizado com sucesso!")
 
                 data_frame_precos_intradiarios = pd.read_csv("src/precos_intradiarios_top_15.csv")
-                tickers_top_15 = data_frame_precos_intradiarios['symbol'].unique().tolist()
+                tickers_top_15 = data_frame_precos_intradiarios["symbol"].unique().tolist()
 
             except Exception as e:
                 st.error(f"Ops, houve um erro ao atualizar: {e}")
-
+    
     if isinstance(data_frame_top_15_industry, pd.DataFrame):
         st.subheader("Top 15 Empresas Listadas")
         display_top_15_table(data_frame_top_15_industry)
@@ -95,3 +155,4 @@ elif page == "Tabela":
     if isinstance(data_frame_precos_intradiarios, pd.DataFrame):
         st.subheader("Preços no período selecionado")
         display_intraday_prices_table(data_frame_precos_intradiarios)
+
