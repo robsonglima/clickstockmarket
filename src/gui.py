9e1b6a94f417a75843fa27ffe3b7120910dfdb94
robsonglima <robsonglima@gmail.com>
2025-02-01 01:10:40 +0000

    fix

--- src/gui.py
+++ src/gui.py
@@ -1,68 +1,59 @@
 import streamlit as st
-from app import *
-from analitics import consultar_precos_intradiarios_yf, preencher_industry
+from app import load_data, display_intraday_prices_table, display_top_15_table, run_app
+import pandas as pd
+from analitics import consultar_precos_intradiarios_yf, preencher_industry, analyze_trend_initiation, get_company_data, show_graph_selected_tickers
 
 st.sidebar.title("Menu")
 
-#Function to format the number
-def format_number(number):
-    if isinstance(number, float):
-        return "{:.2f}".format(number)
-    return number
+# Use radio button in the sidebar for page selection
+page = st.sidebar.radio("Ir para", ["Principal", "Gráfico", "Tabela", "Comparativo"])
 
-#Create the sidebar to navigate between pages.
-if "page" not in st.session_state:
-    st.session_state.page = "Principal"
-
-def change_page(new_page):
-    st.session_state.page = new_page
-
-if st.session_state.page == "Principal":
-    st.title("Análise de Ações")
+if page == "Principal":
+    st.title("Análise de Ações") 
     st.markdown("""
             Bem-vindo ao Análise de Ações!
 
-            Descubra as correlações entre ações e identifique padrões que podem indicar boas oportunidades de investimento. Nossa plataforma permite que você analise o comportamento do mercado em diferentes períodos de tempo, ajudando você a entender como os ativos se movem juntos e quais sinais podem antecipar tendências.
-
-            Use a barra lateral para explorar os dados e melhorar suas estratégias.
-           
-            """
-                )
-
-    # Create the buttons in 3 colums
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-        st.button("Comparativo", on_click=change_page, args=("Comparativo",))
-
-    with col2:
-        st.button("Gráfico", on_click=change_page, args=("Gráfico",))
-
-    with col3:
-        st.button("Tabela", on_click=change_page, args=("Tabela",))
-
-elif st.session_state.page == "Comparativo":
+            Descubra as correlações entre ações e identifique padrões que podem indicar boas oportunidades de investimento. Nossa plataforma permite que você analise o comportamento do mercado em diferentes períodos de tempo, ajudando você a entender como os ativos se movem juntos e quais sinais podem antecipar tendências.          
+            """)
+    
+elif page == "Comparativo":
     st.title("Comparativo")
-    data_frame_top_15_industry, data_frame_precos_intradiarios = load_data('1d', '1y')
+    data_frame_top_15_industry, data_frame_precos_intradiarios = load_data()    
 
     if isinstance(data_frame_top_15_industry, pd.DataFrame):
         tickers_list = data_frame_top_15_industry['TckrSymb'].tolist()
         selected_tickers = st.multiselect("Selecione os Tickers", tickers_list, key="dashboard_tickers")
     else:
-      selected_tickers = []
+        selected_tickers = []
 
     from datetime import date
     start_date = st.date_input("Data Inicial", date(2023, 1, 1))
+    end_date = st.date_input("Data Final", date.today())
 
     company_data_list = []
+    if selected_tickers :
+        for ticker in selected_tickers:
+          company_data = get_company_data(ticker, start_date, end_date)
+          company_data_list.append(company_data)
+        
+        for ticker, company_data in zip(selected_tickers, company_data_list):
+           show_company_info(company_data, ticker)
+        
+        downward_trends, upward_trends = analyze_trend_initiation(selected_tickers, start_date, end_date)    
+        if downward_trends:
+            for ticker, trend_time in downward_trends.items():
+                st.write(f"**Primeira Tendência de Baixa Iniciada:** {ticker} em {trend_time}")
+        else:        
+            st.write(f"**Primeira Tendência de Baixa Iniciada:** Sem tendências detectadas.")
+
+        if upward_trends:
+            for ticker, trend_time in upward_trends.items():
+                st.write(f"**Primeira Tendência de Alta Iniciada:** {ticker} em {trend_time}")
 
     if selected_tickers:
-        show_comparative_graph(selected_tickers, start_date, end_date)
-
         for ticker in selected_tickers:
             company_data = get_company_data(ticker, start_date, end_date)
             company_data_list.append(company_data)
-            
         for ticker, company_data in zip(selected_tickers, company_data_list):
             show_company_info(company_data, ticker)
         downward_trends, upward_trends = analyze_trend_initiation(selected_tickers, start_date, end_date)
@@ -76,42 +67,18 @@ elif st.session_state.page == "Comparativo":
         if upward_trends:
             for ticker, trend_time in upward_trends.items():
                 st.write(f"**Primeira Tendência de Alta Iniciada:** {ticker} em {trend_time}")
-        else:
-            st.write(f"**Primeira Tendência de Alta Iniciada:** Sem tendências detectadas.")
         
 
-elif st.session_state.page == "Gráfico":
-    st.title("Gráfico de Ações")
+elif page == "Gráfico":
+    st.title("Gráfico")
 
-    data_frame_top_15_industry, data_frame_precos_intradiarios = load_data()
-
-    interval_options = ["1min", "2min", "5min", "15min", "30min", "60min", "90min", "1h", "1d"]
-    period_options = ["1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"]
-
-    interval = st.selectbox("Selecione o Intervalo", interval_options, index=8)
-
-    data_frame_top_15_industry, data_frame_precos_intradiarios = load_data(interval, period)
-
-    if isinstance(data_frame_top_15_industry, pd.DataFrame):
-        tickers_list = data_frame_top_15_industry['TckrSymb'].tolist()
-        selected_tickers = st.multiselect("Selecione os Tickers", tickers_list, key="grafico_tickers")
-
-        if selected_tickers:
-            graph_data = data_frame_precos_intradiarios[data_frame_precos_intradiarios['symbol'].isin(selected_tickers)]
-            show_graph_selected_tickers(graph_data, selected_tickers)
-        else:
-            st.write("Selecione pelo menos um ticker para exibir o gráfico.")
-    else:
-        st.write("Não foi possível carregar os dados dos tickers.")
-
-elif st.session_state.page == "Tabela":
-
+elif page == "Tabela":
     st.title("Tabelas de Dados")
 
     interval_options = ["1min", "2min", "5min", "15min", "30min", "60min", "90min", "1h", "1d"]
     period_options = ["1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max",]
 
-    interval = st.selectbox("Selecione o Intervalo", interval_options, index=8)
+    interval = st.selectbox("Selecione o Intervalo", interval_options, index=8)    
     period = st.selectbox("Selecione o Período", period_options, index=5)
 
     data_frame_top_15_industry, data_frame_precos_intradiarios = load_data(interval, period)
@@ -123,5 +90,4 @@ elif st.session_state.page == "Tabela":
     if isinstance(data_frame_precos_intradiarios, pd.DataFrame):
         st.subheader("Preços no período selecionado")
         display_intraday_prices_table(data_frame_precos_intradiarios)
-
 
